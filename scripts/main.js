68606f7362fe13c8b496cec2202f386683e039e8
jlhuang <jlhuang0821@gmail.com>
2019-08-08 15:36:16 +0900

    add pages

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ window.onload = function() {
 document.querySelector("html").onclick = function() {
     let myheading = document.querySelector("h1");
     if (myheading.textContent === "HAPPY FACE") {
-        myheading.textContent = "STOP CLICK ME!";
+        myheading.textContent = "STOP CLICK ME";
     } else {
         myheading.textContent = "HAPPY FACE";
     }

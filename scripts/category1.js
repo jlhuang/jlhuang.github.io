27ec7252c52c32400baf7d4a3642bb190f65534c
jlhuang <jlhuang0821@gmail.com>
2019-08-08 17:42:50 +0900

    Update category1.js

--- scripts/category1.js
+++ scripts/category1.js
@@ -4,6 +4,7 @@ window.onload = function() {
     let category1 = mylist["商品分類1"];
     let i = 1;
     for (let key in category1) {
+        
         document.getElementById("product1"+i).textContent = key;
         document.getElementById("summary1"+i).textContent = category1[key];
         i++;

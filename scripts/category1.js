c641ff1a02952a344a0c202b7ca399439292bac4
jlhuang <jlhuang0821@gmail.com>
2019-08-08 17:39:38 +0900

    update js

--- scripts/category1.js
+++ scripts/category1.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    let mylist = localStorage.getItem("key");
+    let mylist = localStorage.getItem("mylist");
+    mylist = JSON.parse(mylist);
     let category1 = mylist["商品分類1"];
-    alert(JSON.stringify(mylist));
     let i = 1;
     for (let key in category1) {
         document.getElementById("product1"+i).textContent = key;

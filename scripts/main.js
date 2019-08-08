085408eddc4d7396a97a8d2580e1c74e41a8ffbd
jlhuang <jlhuang0821@gmail.com>
2019-08-08 13:34:04 +0900

    update js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 document.querySelector("html").onclick = function() {
     let myheading = document.querySelector("h1");
-    if (myheading.textContent === "MY HEADING") {
-        myheading.textContent = "stop click me!";
+    if (myheading.textContent === "HAPPY FACE") {
+        myheading.textContent = "STOP CLICK ME!";
     } else {
-        myheading.textContent = "MY HEADING";
+        myheading.textContent = "HAPPY FACE";
     }
 };
